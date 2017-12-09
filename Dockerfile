bbe2259090f27a61b8ad3f01fac010f556fbe355
root <root@kalliola-icinga.kalliola.ad>
2017-12-09 20:22:57 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM debian:stretch
+
+MAINTAINER Jere Virta 
+
+ENV DEBIAN_FRONTEND=noninteractive \
+    MASTER_HOST=icinga2
+
+COPY content/ /
+
+RUN apt-get update \
+    && apt-get -qqy install --no-install-recommends \
+       apt-utils \
+       ca-certificates \
+       cron \
+       debian-keyring \
+       ethtool \
+       gnupg \
+       nano \
+       net-tools \
+       procps \
+       smartmontools \
+       snmp \
+       supervisor \
+       sysstat \
+       wget \
+    && wget -O - https://packages.icinga.com/icinga.key | apt-key add - \
+    && apt-get -qqy install --no-install-recommends \
+       icinga2 \
+       monitoring-plugins \
+       monitoring-plugins-basic \
+       monitoring-plugins-common \
+       monitoring-plugins-standard \
+       snmp \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+    
+
+RUN chmod +x /opt/start.sh \
+	     /opt/supervisor/*
+
+EXPOSE 5665
+
+ENTRYPOINT ["/opt/start.sh"]
+    
+    
---
+++ content/opt/start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Icinga2 installed, configuring..."
+
+PKI_DIR="/etc/icinga2/pki"
+
+echo "$MASTER_IP $MASTER_HOST" >>/etc/hosts
+echo "127.0.2.1 $CLIENT_HOST" >>/etc/hosts
+
+chown nagios.nagios /etc/icinga2 -R
+
+rm -rf /etc/icinga2/conf.d/*
+
+icinga2 pki new-cert --cn $CLIENT_HOST --key $PKI_DIR/$CLIENT_HOST.key --cert $PKI_DIR/$CLIENT_HOST.crt
+icinga2 pki save-cert --key $PKI_DIR/$CLIENT_HOST.key --cert $PKI_DIR/$CLIENT_HOST.crt --trustedcert $PKI_DIR/trusted-cert.crt --host $MASTER_HOST
+icinga2 node setup --ticket $ICINGA_TICKET --zone $CLIENT_HOST --master_host $MASTER_HOST  --trustedcert  $PKI_DIR/trusted-cert.crt  --cn $CLIENT_HOST  --endpoint $MASTER_HOST,$MASTER_IP,5665 --accept-commands --accept-config
+
+sed -i '$ i object Zone "global-templates" { global = true }' /etc/icinga2/zones.conf
+sed -i '$ i object Zone "director-global" { global = true }' /etc/icinga2/zones.conf
+
+chmod u+s,g+s /bin/ping
+chmod u+s,g+s /bin/ping6
+chmod u+s,g+s /usr/lib/nagios/plugins/check_icmp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Start supervisord
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n &
+trap "supervisorctl shutdown && wait" SIGTERM
+wait
